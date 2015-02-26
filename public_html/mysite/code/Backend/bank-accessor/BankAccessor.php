a12463478df05a5519f28d4fec8785755b72960a
martin-smith <martin19942010@gmail.com>
2015-02-26 21:05:30 +0000

    added code to stop SQL injection into the login query

--- public_html/mysite/code/Backend/bank-accessor/BankAccessor.php
+++ public_html/mysite/code/Backend/bank-accessor/BankAccessor.php
@@ -6,7 +6,31 @@ class BankAccessor implements BankInterface {
 		
 		//check for SQL injection 
 		// get pass where username = ""
+		$databasePass = User::get()->byUsername( $username )->Password();
 		//Send password to be decrypted
+		if(checkPasswordMobile($databasePass, $passwordBits, $indexes)){
+		
+			$user = User::get()->byUsername( $username);
+			$accounts = $user->Accounts();
+			
+			$products = DB::query('SELECT P.Title, P.Content 
+								   FROM product P 
+                                   Where P.ID NOT IN  
+								   (Select product.ID 
+								   From product, user, account
+								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> ' . Convert::raw2sql($username)  . ' )')->toArray();
+								   
+								   
+			
+			
+
+			return new LoginOutput($user, $accounts, $products, generateToken(), true);
+		
+		}else{
+			
+			return new LoginOutput(null, null, null, null);
+		
+		}
 		//If they are equal
 			// generate token
 			// send back loginOutput
@@ -32,7 +56,7 @@ class BankAccessor implements BankInterface {
                                    Where P.ID NOT IN  
 								   (Select product.ID 
 								   From product, user, account
-								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> '$username ' )')->toArray();
+								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> ' . Convert::raw2sql($username)  . ' )')->toArray();
 								   
 								   
 			
@@ -50,9 +74,8 @@ class BankAccessor implements BankInterface {
 			// send back loginOutput
 		//else
 			// send back unsuccessful
-	
-	
 	}
+	
 	
 	public function loadTransactions( $userID, $accountID, $month, $year, $token ){
 	
@@ -69,18 +92,17 @@ class BankAccessor implements BankInterface {
 	
 	private function checkPassword( $databasePassword, $givenPassword){
 	
-		$return = true;
+		
 		// call the decrypt of password 
 		$plaindatabasePassword = decrypt($databasePassword);
 		// check return
 		// pass back fail/pass
 		if( strcmp($plaindatabasePassword, $givePassword) === 0){
 			$plaindatabasePassword = null;
-			return $result;
+			return true;
 		}else{
 			$plaindatabasePassword = null;
-			$result = false;
-			return $result;
+			return false;
 		
 		}
 	
@@ -92,7 +114,22 @@ class BankAccessor implements BankInterface {
 	private function checkPasswordMobile( $databasePassword, $givenPassword, $digits){
 	
 		// call the decrypt of password 
+		$plaindatabasePassword = decrypt($databasePassword);
 		// check return with positions
+		
+		if(strcmp($plaindatabasePassword{digits[0]},$givenPassword{0}) ===0 && 
+		   strcmp($plaindatabasePassword{digits[1]},$givenPassword{1})===0 && 
+		   strcmp($plaindatabasePassword{digits[2]},$givenPassword{2})===0){
+		
+			$plaindatabasePassword = null;
+			return true;
+		}else{
+			$plaindatabasePassword = null;
+			return false;
+		
+		}
+		
+		
 		// pass back fail/pass
 	
 	}
