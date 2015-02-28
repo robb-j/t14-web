06624070e30a7a30312ae9812c44b9eef81ebfbd
martin-smith <martin19942010@gmail.com>
2015-02-28 19:16:16 +0000

    Login functionality works for both mobile and web, Products however still don't work Random strings of length 64 will be generated for the token

--- public_html/mysite/code/Backend/bank-accessor/BankAccessor.php
+++ public_html/mysite/code/Backend/bank-accessor/BankAccessor.php
@@ -2,15 +2,30 @@
 
 class BankAccessor implements BankInterface {
 
+	
+
 	public function loginFromMobile( $username, $passwordBits, $indexes ){
 		
 		//check for SQL injection 
-		// get pass where username = ""
-		$databasePass = User::get()->byUsername( $username )->Password();
-		//Send password to be decrypted
-		if(checkPasswordMobile($databasePass, $passwordBits, $indexes)){
+		$sanitisedUsername = Convert::raw2sql($username);
 		
-			$user = User::get()->byUsername( $username);
+		// get pass where username = ""
+		$databasePass = User::get()->filter(array(
+			'Username' => $sanitisedUsername
+		))[0];
+		
+		//If there is no user then you can't get the password of a null value
+		if($databasePass !== null){
+			$databasePass = $databasePass->Password;
+		}
+		
+		//Send password to be decrypted
+		if( strlen($passwordBits) === 3 && sizeof($indexes)===3 && $this->checkPasswordMobile($databasePass, $passwordBits, $indexes) ){
+		
+			$user = User::get()->filter(array(
+			'Username' => $sanitisedUsername
+			))[0];
+			
 			$accounts = $user->Accounts();
 			
 			$products = DB::query('SELECT P.Title, P.Content 
@@ -18,13 +33,13 @@ class BankAccessor implements BankInterface {
                                    Where P.ID NOT IN  
 								   (Select product.ID 
 								   From product, user, account
-								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> ' . Convert::raw2sql($username)  . ' )')->toArray();
+								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> \'' . Convert::raw2sql($sanitisedUsername)  . '\' )');
 								   
 								   
 			
 			
 
-			return new LoginOutput($user, $accounts, $products, generateToken(), true);
+			return new LoginOutput($user, $accounts, $products, $this->generateToken(), true);
 		
 		}else{
 			
@@ -43,28 +58,44 @@ class BankAccessor implements BankInterface {
 	public function login( $username, $password){
 	
 		//check for SQL injection 
-		// get pass where username = ""
-		$databasePass = User::get()->byUsername( $username )->Password();
-		//Send password to be decrypted
-		if(checkPassword($databasePass, $password)){
+		$sanitisedUsername = Convert::raw2sql($username);
 		
-			$user = User::get()->byUsername( $username);
+		$databasePass = User::get()->filter(array(
+			'Username' => $sanitisedUsername
+		))[0];
+		
+		//If there is no user then you can't get the password of a null value
+		if($databasePass !== null){
+			$databasePass = $databasePass->Password;
+		}
+
+		//Send password to be decrypted
+		if($this->checkPassword($databasePass, $password) === true){
+		
+			//This returns the first user as we are assuming there is no duplicate usernames
+			$user = User::get()->filter(array(
+			'Username' => $sanitisedUsername
+			))[0];
+			
+			//This returns a HasManyList use [x] to access elements
 			$accounts = $user->Accounts();
 			
+			/*
+			*
+			* This is broken not sure how to fix it 
+			*
+			*/
 			$products = DB::query('SELECT P.Title, P.Content 
-								   FROM product P 
+								   FROM Product P 
                                    Where P.ID NOT IN  
-								   (Select product.ID 
-								   From product, user, account
-								   Where product.ID = account.ProductID AND user.ID = account.UserID AND user.username <> ' . Convert::raw2sql($username)  . ' )')->toArray();
-								   
-								   
-			
-			
+								   (Select Product.ID 
+								   From Product, User, Account
+								   Where Product.ID = Account.ProductID AND User.ID = Account.UserID AND User.Username <> \'' . $sanitisedUsername  . '\' )');
 
-			return new LoginOutput($user, $accounts, $products, generateToken(), true);
+			return new LoginOutput($user, $accounts, $products, $this->generateToken(), true);
 		
 		}else{
+
 			
 			return new LoginOutput(null, null, null, null);
 		
@@ -92,12 +123,11 @@ class BankAccessor implements BankInterface {
 	
 	private function checkPassword( $databasePassword, $givenPassword){
 	
-		
 		// call the decrypt of password 
-		$plaindatabasePassword = decrypt($databasePassword);
+		$plaindatabasePassword = $this->decrypt($databasePassword);
 		// check return
 		// pass back fail/pass
-		if( strcmp($plaindatabasePassword, $givePassword) === 0){
+		if( strcmp($plaindatabasePassword, $givenPassword) === 0){
 			$plaindatabasePassword = null;
 			return true;
 		}else{
@@ -114,7 +144,7 @@ class BankAccessor implements BankInterface {
 	private function checkPasswordMobile( $databasePassword, $givenPassword, $digits){
 	
 		// call the decrypt of password 
-		$plaindatabasePassword = decrypt($databasePassword);
+		$plaindatabasePassword = $this->decrypt($databasePassword);
 		// check return with positions
 		
 		if(strcmp($plaindatabasePassword{$digits[0]},$givenPassword[0]) ===0 && 
@@ -144,11 +174,18 @@ class BankAccessor implements BankInterface {
 	
 	private function generateToken(){
 	
-		$token = "xl1ff9ndndj3jdmd9dn2nw91nx";
+		$length =64;
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$token = '';
+
+		for ($i = 0; $i < $length; $i++) {
+			$token .= $characters[mt_rand(0, strlen($characters) - 1)];
+		}
+
+    
+
+		echo"|".$token."|";
 		return $token;
-	
 	}
-	
-	
 }
 ?>
