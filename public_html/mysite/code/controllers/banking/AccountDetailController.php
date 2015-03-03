711eaac41a9657562cdcd4637e90f237eb9c49da
yifan wei <nlwarrior@gmail.com>
2015-03-03 01:37:22 +0000

    Setting up Account Detail Page

---
+++ public_html/mysite/code/controllers/banking/AccountDetailController.php
@@ -0,0 +1,20 @@
+<?php
+
+/* A Page that displays a User's Accounts
+ * The first page they'll see once thay logged in
+ * Created by Rob A - feb 2015
+ */
+class AccountDetailController extends BankController {
+	
+	
+	// Set the tab title
+	public $TabTitle = "banking";
+	
+	
+	
+	public function Content() {
+		
+	// Render the Account template
+	return "Hello World, I made a controller";
+	}
+}
