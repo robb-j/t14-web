f1024784eaf07c63f7c7ae289ff0b9403b9aeeb5
yifanwei <nlwarrior@gmail.com>
2015-04-04 17:30:18 +0100

    Claims page completed

--- public_html/mysite/code/controllers/rewards/ClaimController.php
+++ public_html/mysite/code/controllers/rewards/ClaimController.php
@@ -2,6 +2,7 @@
 
 /* A Page that lets the user claim a reward
  * Created by Rob A - March 2015
+ * Completed by Yifan W - April 2015
  */
 class ClaimController extends BankController {
 	
@@ -11,6 +12,12 @@ class ClaimController extends BankController {
 	
 	
 	public function Content() {
+		
+		// Create an API to access the database
+		$api = new WebApi();
+		
+		// Get the list of available rewards for the user
+		$this->Rewards = $api->getAllRewards()->limit(7);
 		
 		// Render with a template
 		return $this->renderWith("ClaimContent");
