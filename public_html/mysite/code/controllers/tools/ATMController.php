5d3f74effe9e49e581fafdad5c7234f040f069be
martin-smith <martin19942010@gmail.com>
2015-04-12 23:07:50 +0100

    attempted to get ATM Map working

--- public_html/mysite/code/controllers/tools/ATMController.php
+++ public_html/mysite/code/controllers/tools/ATMController.php
@@ -7,8 +7,30 @@ class ATMController extends ToolController {
 	
 	public $ToolName = "atm";
 	
+	private static $allowed_actions = array(
+		"loadATMs"
+	);
+	
 	public function ToolContent() {
+		$api = new WebApi();
+	
+		$this->atmList = $api->loadATMs($this->CurrentUser->ID);
 		
 		return $this->renderWith("ATMTool");
+	}
+	
+	public function loadATMs(){
+	
+		// Create an API to access the database
+		$api = new WebApi();
+	
+		$arrayOfATMObjects = $api->loadATMs($this->CurrentUser->ID);
+		
+		foreach ($arrayOfATMObjects as $ATM){
+		
+			echo " Title=".$ATM->Title." Cost=".$ATM->Cost." Lng=".$ATM->Longitude." Lat=".$ATM->Latitude;
+		
+		}
+		return 0;
 	}
 }
